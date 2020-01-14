df26a31b76cb3b2cc47717b79e2d3319bfab4397
Wolski <merito123@gmail.com>
2020-01-15 00:03:53 +0100

    Add local storage support

--- image_resizer/image_resizer.py
+++ image_resizer/image_resizer.py
@@ -14,30 +14,85 @@ ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-S3_BUCKET = env.get('S3_BUCKET')
+STORE_PROPERTY = env.get('S3_BUCKET')
 
 logging.basicConfig(level=logging.INFO)
 
 
+class GenericFile:
+    def __init__(self, body_string):
+        self._body_string = body_string
+        self._thumbnail_name, self._mimetype = self.parse_filename(self._body_string)
+        self._full_handler = None
+        self._thumbnail_handler = None
+
+    def __del__(self):
+        self._full_handler.close()
+
+    def get_file(self):
+        pass
+
+    def save_thumbnail(self):
+        pass
+
+    @staticmethod
+    def parse_filename(body_string):
+        file_name = body_string.rsplit('.', 1)[0]
+        file_extension = body_string.rsplit('.', 1)[1]
+        thumbnail_name = file_name + '_m.' + file_extension
+        mimetype = 'image/png' if file_extension == 'png' else 'image/jpeg'
+        return thumbnail_name, mimetype
+
+    def resize_image(self):
+        with Image.open(self._full_handler) as image:
+            img = resizeimage.resize_contain(image, [385, 385]).convert('RGB')
+            img.save(self._thumbnail_handler, image.format)
+            self._thumbnail_handler.seek(0)
+
+
+class LocalFile(GenericFile):
+    def __init__(self, body_string, path):
+        self._path = path
+        super().__init__(body_string=body_string)
+
+    def get_file(self):
+        self._full_handler = open(os.path.join(self._path, self._body_string), 'rb')
+
+    def save_thumbnail(self):
+        with open(os.path.join(self._path, self._thumbnail_name), 'wb') as self._thumbnail_handler:
+            self.resize_image()
+
+
+class S3File(GenericFile):
+    def __init__(self, body_string, bucket):
+        self._bucket = bucket
+        self._s3_client = boto3.client('s3')
+        super().__init__(body_string=body_string)
+
+    def get_file(self):
+        self._full_handler = tempfile.TemporaryFile()
+        self._s3_client.download_fileobj(self._bucket, self._body_string, self._full_handler)
+
+    def save_thumbnail(self):
+        with tempfile.TemporaryFile() as self._thumbnail_handler:
+            self.resize_image()
+            self._s3_client.upload_fileobj(self._thumbnail_handler, self._bucket, self._thumbnail_name,
+                                           ExtraArgs={'ACL': 'public-read', 'ContentType': self._mimetype})
+
+
 def callback(ch, method, properties, body):
     body_string = body.decode("utf-8")
-    file_name = body_string.rsplit('.', 1)[0]
-    file_extension = body_string.rsplit('.', 1)[1]
-    mimetype = 'image/png' if file_extension == 'png' else 'image/jpeg'
-    s3_client = boto3.client('s3')
-    with tempfile.TemporaryFile() as fp:
-        logging.info(body_string)
-        s3_client.download_fileobj(S3_BUCKET, body_string, fp)
-        fp_m = tempfile.TemporaryFile()
-        try:
-            with Image.open(fp) as image:
-                img = resizeimage.resize_contain(image, [385, 385]).convert('RGB')
-                img.save(fp_m, image.format)
-                fp_m.seek(0)
-            s3_client.upload_fileobj(fp_m, S3_BUCKET, file_name + '_m.' + file_extension, ExtraArgs={'ACL': 'public-read', 'ContentType': mimetype})
-            fp_m.close()
-            logging.info(body_string)
-        except IOError: # in case of input file malfunction
-            logging.info("File broken " + body_string)
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    store_property = STORE_PROPERTY.split('//', 1)[1]
 
+    if STORE_PROPERTY.startswith('file://'):
+        thumbnail = LocalFile(body_string=body_string, path=store_property)
+    elif STORE_PROPERTY.startswith('s3://'):
+        thumbnail = S3File(body_string=body_string, bucket=store_property)
+
+    try:
+        thumbnail.get_file()
+        thumbnail.save_thumbnail()
+        logging.info(body_string)
+    except IOError: # in case of input file malfunction
+        logging.info("File broken " + body_string)
+    ch.basic_ack(delivery_tag = method.delivery_tag)

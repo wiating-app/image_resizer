c97dcc73127e77f7d39eb155f9f4a900e9a118d5
Wolski <merito123@gmail.com>
2020-04-18 20:32:46 +0200

    Fix celery init attrs

--- image_resizer/__main__.py
+++ image_resizer/__main__.py
@@ -12,6 +12,4 @@ STORE_PROPERTY = env.get('S3_BUCKET')
 
 logging.basicConfig(level=logging.INFO)
 
-QUEUE_NAME = env.get('IMAGE_RESIZER_QUEUE')
-
-app = Celery(QUEUE_NAME, broker='amqp://localhost')
+app = Celery('image_resizer', broker='amqp://localhost')

45925304c168389e5e1b0e8093c90f78ea6e026c
Wolski <merito123@gmail.com>
2020-04-18 21:10:46 +0200

    Add all variable

--- image_resizer/__init__.py
+++ image_resizer/__init__.py
@@ -6,4 +6,7 @@ __author__ = """Dawid Wolski"""
 __email__ = 'merito123@gmail.com'
 __version__ = '0.1.0'
 
-from .__main__ import resize_image
+from .__main__ import resize_image, celery
+
+
+__all__ = ['resize_image', 'celery']

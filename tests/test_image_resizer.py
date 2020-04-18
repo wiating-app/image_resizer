a4062d2523d9b6e8a8831ca0b5ab27088f3f6df3
Wolski <merito123@gmail.com>
2020-04-18 21:03:45 +0200

    Move code to the main

--- tests/test_image_resizer.py
+++ tests/test_image_resizer.py
@@ -2,21 +2,3 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `image_resizer` package."""
-
-
-import unittest
-
-from image_resizer import image_resizer
-
-
-class TestImage_resizer(unittest.TestCase):
-    """Tests for `image_resizer` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
